70a59df5fa5c37a178de6e7c744d3d028c40d49d
fu_Mac <fuhuicong@qq.com>
2017-08-20 22:19:07 +0800

    Initial Commit

---
+++ AGuideToIosAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGuideToIosAnimation
+//
+//  Created by fu on 2017/8/20.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AGuideToIosAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGuideToIosAnimation
+//
+//  Created by fu on 2017/8/20.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
